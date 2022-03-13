1c55a3fb8213dc39ca65365df1f9c6404de26466
dick <dick@gmail.com>
2022-03-13 06:58:42 +0000

    add 006 script

---
+++ script_006_positional_parameters/positional_parameters.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Author : dick
+# Date Created : 2022/03/13
+# Last Modified : 2022/03/13
+
+# Description
+# Get positional parameters ,2 digits parameters should be braced
+
+# Usage
+# positional parameters
+
+echo "My name is $1"
+echo "My home directory is $2"
+echo "My favourite colour is $3"
+echo "The 10th argument is ${10}"
+
+exit 0
