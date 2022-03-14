82b7674f3602b3300cdfb2658b58a1a0c82e1fd0
dick <dick@gmail.com>
2022-03-14 14:30:37 +0000

    add script 020

---
+++ script_020_scp_local_to_gcp/scp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author : dick
+# Date Created : 2022/03/14
+# Last Modified : 2022/03/14
+
+# Description
+# use scp to upload files or scripts from local to gcp vm
+
+# Usage
+# scp scp.sh root@34.81.190.187:~/
+
+curl ifconfig.co > ~/ip.txt
+
+exit 0
